5f91cb27660ce34fb64894fb8b2b0c49c4590530
Carlos Astudillo (CEAMDEV) <146150693+ceamdev@users.noreply.github.com>
2024-08-02 21:36:16 -0400

    Update server.php

--- config/server.php
+++ config/server.php
@@ -1,7 +1,7 @@
 <?php
 /** Datos de conexion a base de datos, bajo constantes. */
     const DB_SERVER="localhost";
-    const DB_NAME="cyspaymedb";
+    const DB_NAME="basededatos";
     const DB_USER="root";
     const DB_PASS="";
     const DB_PORT="3306";
